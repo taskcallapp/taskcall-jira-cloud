745a0948051ae5dfc785f997eb2201814a5c91ff
root <root@ip-172-31-45-104.eu-west-3.compute.internal>
2021-09-24 19:34:58 +0000

    Updated app

---
+++ static/taskcall/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { invoke } from '@forge/bridge';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  }, []);
+
+  return (
+    <div>
+	  {
+	      data ?
+		  data
+		  :
+		  <div>
+		      <button> Create Incident </button>
+		  </div>
+	  }
+    </div>
+  );
+}
+
+export default App;
