d2ae30f0e638d9da541d90e305aaf7278ea75ece
riasatullah <riasat.ullah@gmail.com>
2021-09-27 21:20:36 +0200

    changes

--- static/taskcall/src/App.js
+++ static/taskcall/src/App.js
@@ -15,79 +15,9 @@ function App() {
 
   return (
     <div>
-    {
-      data ?
-		  <div>
-        <label id="lblTcInc" hidden></label>
-        <p id="pError" style={{ padding: "10px", color: "red", fontSize: "12px", textAlign: "center" }} hidden></p>
-
-        <div style="clear: both;">
-          <div class="header-text" style="width: 75%; float: left;">
-            <div id="divStatus" class="status-tag"> Open </div>
-            <div style="margin-top: 2px;">
-              <a href="https://app.taskcallapp.com/incidents/{{ data.incident_title }}" target="_blank"> Test Incident Cinco </a>
-            </div>
-          </div>
-            {
-              true ?
-              <div style={{ width: "20%", float: "left", paddingLeft: "3%" }}>
-                <button id="btnGroupDrop1" type="button">
-                  <span> <img src="menu.svg"/> </span>
-                </button>
-                <div id="divActionsMenu" class="dropdown-menu app-text"></div>
-              </div>
-              : null
-            }
-        </div>
-
-        <div style={{ clear: "both", height: "8px" }}></div>
-
-        <div style={{ clear: "both", marginBottom: "8px" }}>
-          <a class="btn btn-link header-text px-0">
-            { _lu.ttl_details }
-          </a>
-
-          <div class="collapse show" id="collapseDetails">
-              <div class="app-text">
-                  <table>
-                      <tr class="table-row">
-                          <td class="table-attribute-col"> { _lu.det_service }: </td>
-                          <td id="tdService"></td>
-                      </tr>
-                      <tr class="table-row">
-                          <td class="table-attribute-col"> { _lu.det_urgency }: </td>
-                          <td id="tdUrgency"></td>
-                      </tr>
-                      <tr class="table-row">
-                          <td class="table-attribute-col"> { _lu.det_assigned_to }: </td>
-                          <td id="tdAssignedTo"></td>
-                      </tr>
-                      <tr class="table-row">
-                          <td class="table-attribute-col"> { _lu.det_next_alert_in }: </td>
-                          <td id="tdNextAlertIn"></td>
-                      </tr>
-                  </table>
-              </div>
-          </div>
-
-          <div style={{ clear: "both", marginBottom: "8px", paddingBottom: "20px" }}>
-              <a class="btn btn-link header-text px-0">
-                  { _lu.ttl_timeline }
-              </a>
-              <div class="collapse show" id="collapseTimeline">
-                  <div id="timelineBody" class="app-text">
-                  </div>
-              </div>
-          </div>
-        </div>
-      </div>
-		  :
-      <div>
-        <Button appearance="primary">
-          { _lu.ttl_create_incident }
-        </Button>
-      </div>
-	  }
+      <Button appearance="subtle">
+        { _lu.ttl_create_incident }
+      </Button>
     </div>
   );
 }
