375cb66aed9d12f1ef5738f82a93ee90e47a6dc3
riasatullah <riasat.ullah@gmail.com>
2021-09-27 18:37:50 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Resolver from '@forge/resolver';
+import * as VarNames from '../src/VarNames';
 
 const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
   console.log(req);
 
-  return 'Hello, world!';
+  var data = {
+    "incident_title": "Test incident 5",
+    "status": "Open",
+    "toShowActionOptions": true
+  }
+  return data;
 });
 
 export const handler = resolver.getDefinitions();
--- static/taskcall/src/App.js
+++ static/taskcall/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
 import * as _lu from '../src/assets/files/label_universe.json';
+import * as VarNames from '../src/VarNames';
 
 
 function App() {
@@ -15,12 +16,33 @@ function App() {
     {
       data ?
 		  <div>
-        <button style="background-color: #4CBB17; color: white; border-radius: 3px;"> { _lu.ttl_create_incident } </button>
+        <label id="lblTcInc" hidden></label>
+        <p id="pError" style={{ padding: "10px", color: "red", fontSize: "12px", textAlign: "center" }} hidden></p>
+
+        <div style="clear: both;">
+          <div class="header-text" style="width: 75%; float: left;">
+            <div id="divStatus" class="status-tag"> {{ data.status }} </div>
+            <div style="margin-top: 2px;">
+              <a href="https://app.taskcallapp.com/incidents/{{ data.incident_title }}" target="_blank"> {{ data['incident_title'] }} </a>
+            </div>
+          </div>
+            {
+              data.toShowOptions ?
+              <div style={{ width: "20%", float: "left", paddingLeft: "3%" }}>
+                <button id="btnGroupDrop1" type="button">
+                  <span> <img src="menu.svg"/> </span>
+                </button>
+                <div id="divActionsMenu" class="dropdown-menu app-text"></div>
+              </div>
+              : null
+            }
+        </div>
+
       </div>
 		  :
-		  <div>
-        <button> { _lu.det_service } </button>
-		  </div>
+      <div>
+        <button style={{ backgroundColor: "#4CBB17"; color: "white", borderWidth: "0px", borderRadius: "3px" }}> { _lu.ttl_create_incident } </button>
+      </div>
 	  }
     </div>
   );
