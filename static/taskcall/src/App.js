c721fb9f93ec059fc378c4364b0493a976a0679e
riasatullah <riasat.ullah@gmail.com>
2021-10-02 17:09:45 +0200

    changes

--- static/taskcall/src/App.js
+++ static/taskcall/src/App.js
@@ -21,15 +21,26 @@ function App() {
           <label id="lblTcInc" hidden></label>
           <p id="pError" style={{ padding: "10px", color: "red", fontSize: "12px", textAlign: "center" }} hidden></p>
 
-          <div style={{ height: "100px" }}>
-            <div style={{ flex: 4, backgroundColor: "yellow" }}></div>
-            <div style={{ flex: 1, backgroundColor: "green" }}></div>
+          <div>
+            <div style={{ width: "75%", float: "left", height: "100px" }}>
+              <div id="divStatus" style={{ textTransform: "uppercase" }}> Acknowledged </div>
+              <div style="margin-top: 2px;">
+                  <a href="https://app.taskcallapp.com/incidents/{{ incident_id }}" target="_blank"> New issue has been found. Please check. </a>
+              </div>
+            </div>
+
+            <div style={{ width: "20%", float: "left", paddingLeft: "3%", height: "100px" }}>
+              <button id="btnGroupDrop1" type="button" class="btn btn-light" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span> <img src="../src/assets/images/menu.svg"> </span>
+              </button>
+              <div id="divActionsMenu" class="dropdown-menu app-text" aria-labelledby="btnGroupDrop1"></div>
+            </div>
           </div>
 
-          <div style={{ height: "200px", backgroundColor: "blue" }}>
+          <div style={{ height: "200px" }}>
           </div>
 
-          <div style={{ height: "300px", backgroundColor: "black" }}>
+          <div style={{ height: "300px" }}>
           </div>
         </div>
         :
