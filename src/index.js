4bf2e2f884b5faf49e693f8326a0a3c783e6694c
riasatullah <riasat.ullah@gmail.com>
2021-10-06 15:27:08 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Resolver from '@forge/resolver';
 import * as VarNames from '../src/VarNames';
+import axios from 'axios';
+import api, { requestJira, route } from '@forge/api';
+
+export const BASE_URL = `https://jsonplaceholder.typicode.com/`;
+const API = axios.create({ baseURL: BASE_URL });
 
 const resolver = new Resolver();
 
@@ -12,6 +17,67 @@ resolver.define('getText', (req) => {
     "toShowActionOptions": true
   }
   return data;
+});
+
+resolver.define('getIssue', async (req) => {
+  try {
+    const issueId = req.context.extension.issue.key;
+    const response = await api
+      .asApp()
+      .requestJira(
+        route`/rest/api/3/issue/${issueId}?fields=summary,description`,
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        }
+      );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// this resolver helps update summary and description by taking in updated summary and description as payload
+// https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-issueidorkey-put
+
+resolver.define('updateIssue', async (req) => {
+  try {
+    const { summary, description } = req.payload;
+
+    const issueId = req.context.extension.issue.key;
+    const response = await api
+      .asApp()
+      .requestJira(route`/rest/api/2/issue/${issueId}`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          fields: {
+            description,
+            summary,
+          },
+        }),
+      });
+    console.log(response.status);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+resolver.define('getPostData', async (req) => {
+  const { postId } = req.payload;
+  try {
+    const res = await API.get(`/posts/${postId}`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
 });
 
 export const handler = resolver.getDefinitions();
